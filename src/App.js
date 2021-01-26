84a9fddd93b7072a11b6dd6b2813eb5089bf1152
Liliia Novikova <kplily85@gmail.com>
2021-01-26 21:47:35 +0200

    add nav

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import s from './App.module.css';
-import ContactForm from './ContactForm/ContactForm';
-import Filter from './Filter/Filter';
-import ContactList from './ContactList/ContactList';
+import AppBar from '../src/AppBar/AppBar';
 
 export default function App() {
   return (
     <div className={s.container}>
-      <h1 className={s.title}>Phonebook</h1>
-      <ContactForm />
-
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactList />
+      <AppBar />
     </div>
   );
 }
